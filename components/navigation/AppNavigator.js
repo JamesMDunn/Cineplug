21821badda9d722a5a19770276ac9c373993f2b9
James Dunn <jamesdunn@Jamess-MBP.home>
2019-12-26 16:49:33 -0500

    fixed navigation, to increase performance and switch navigation between home screen and movie screen

--- components/navigation/AppNavigator.js
+++ components/navigation/AppNavigator.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from '../screens/HomeScreen';
+import MoviePage from '../screens/MoviePage';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
 
-import BottomTabNavigator from './BottomTabNavigator';
+const movieFlow = createStackNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
 
-export default createAppContainer(
-  createSwitchNavigator({
-    Main: BottomTabNavigator,
-  }),
-);
+    navigationOptions: {header: null},
+  },
+  MoviePage: {
+    screen: MoviePage,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#141414',
+      },
+      headerTintColor: 'white',
+    },
+  },
+});
+
+movieFlow.navigationOptions = {
+  tabBarIcon: <FontAwesomeIcon icon={faHome} color={'grey'} />,
+};
+
+const switchNavigator = createSwitchNavigator({
+  mainFlow: createBottomTabNavigator(
+    {
+      Home: movieFlow,
+    },
+    {
+      tabBarOptions: {
+        showLabel: true,
+        style: {
+          shadowColor: 'rgba(58,55,55,0.1)',
+          shadowOffset: {width: 0, height: 0},
+          shadowOpacity: 1,
+          fontColor: 'white',
+          elevation: 3,
+          borderTopColor: 'transparent',
+          backgroundColor: '#141414',
+          height: 50,
+        },
+        labelStyle: {
+          color: 'white',
+        },
+      },
+    },
+  ),
+});
+export default createAppContainer(switchNavigator);
