b5867df144af673b631647e64079fe97c5dabf3e
James Dunn <jamesdunn@Jamess-MBP.home>
2019-12-27 22:44:31 -0500

    fixed ios build

--- ios/CinePlug/AppDelegate.m
+++ ios/CinePlug/AppDelegate.m
@@ -17,7 +17,7 @@
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"Movie"
+                                                   moduleName:@"CinePlug"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
